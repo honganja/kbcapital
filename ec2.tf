4c47b45ba415a3167347908a8ac18fb74cd0792f
honganja <55674766+honganja@users.noreply.github.com>
2023-01-04 15:58:19 +0900

    Create ec2.tf
    2번째 web서버생성

---
+++ ec2.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "kbcapital-web2" {
+  ami = "ami-0574da719dca65348"
+  instance_type = "t2.micro"
+  
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Helo, Word" > index.html
+                nohub busybox httpd -f -p 8080 &
+                EOF
+  
+  tags = {
+    Name = "kbcapital-web2"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+  from_port = 8080
+  to_port = 8080
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
