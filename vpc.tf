4631208dfb18b7e74702bdca48eb7ac05a04aaca
honganja <55674766+honganja@users.noreply.github.com>
2023-01-04 17:01:37 +0900

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
-resource "aws_vpc" "kbcapital" {
-  cidr_block       = "10.0.0.0/16"
+resource "aws_vpc" "kbcapital-develop" {
+  cidr_block       = "10.11.0.0/16"
 
   tags = {
     Name = "kbcapital-test"
