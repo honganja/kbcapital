7be953601f28b0cb0e62231fe7db6f37ca5e5db9
honganja <55674766+honganja@users.noreply.github.com>
2023-01-04 17:04:17 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "kbcapital-web1" {
+resource "aws_instance" "kbcapital-prod-web1" {
   ami = "ami-065a2f464074b5337"
   instance_type = "t2.micro"
   
   tags = {
-    Name = "kbcapital-web1"
+    Name = "kbcapital-prod-web1"
   }
 }
