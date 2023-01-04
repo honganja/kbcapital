f2bb207bb4a2854affc3ee0fa03edbe71619a71e
honganja <55674766+honganja@users.noreply.github.com>
2023-01-04 16:36:56 +0900

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "ap-az1" {
-  vpc_id     = aws_vpc.kbcapital.id
+  vpc_id     = aws_vpc.kbcapital_prod.id
   cidr_block = "10.0.1.0/24"
 
   availability_zone = "us-east-1a"
@@ -11,7 +11,7 @@ resource "aws_subnet" "ap-az1" {
 
 
 resource "aws_subnet" "ap-az3" {
-  vpc_id     = aws_vpc.kbcapital.id
+  vpc_id     = aws_vpc.kbcapital_prod.id
   cidr_block = "10.0.2.0/24"
 
   availability_zone = "us-east-1c"
